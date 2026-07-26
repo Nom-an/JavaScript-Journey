6633caf4a39fda72f6c5233941683513b1f4451e
Nom-an <nomanvi123@gmail.com>
2026-07-26 08:47:58 +0600

    typecusting

---
+++ number.js
@@ -0,0 +1,6 @@
+var applePrice = 20;
+var chocolatePrice = parseInt( '20');
+
+console.log(typeof chocolatePrice);
+
+console.log(applePrice + chocolatePrice);
--- variable.js
+++ variable.js
@@ -1,3 +1,8 @@
 var name = "Noman";
 console.log(name);
 console.log(typeof(name));
+
+var passed = true;
+console.log(passed)
+ var isRich = false;
+ console.log(typeof(isRich));
