f4fbe911639dd098d68541e1cea17f796896ce1c
Nom-an <nomanvi123@gmail.com>
2026-07-26 08:53:17 +0600

    tofixed uses

--- number.js
+++ number.js
@@ -4,3 +4,13 @@ var chocolatePrice = parseInt( '20');
 console.log(typeof chocolatePrice);
 
 console.log(applePrice + chocolatePrice);
+
+// using tofix
+
+
+var fistNumber = 0.2;
+var secondNumber = 0.1;
+
+var total = fistNumber + secondNumber;
+
+console.log(total.toFixed(3));
