3b15f516f09202bf7bf80b8b1613eb637b0141d3
Nom-an <nomanvi123@gmail.com>
2026-07-26 20:08:41 +0600

    using of !=

---
+++ shortcut.js
@@ -0,0 +1,20 @@
+var isLeader = true;
+
+if ( !isLeader){
+    console.log("release the baby ");
+}
+else{
+    console.log("nothing will happen");
+}
+
+// using of != (not )
+
+
+var passed= false;
+
+if (!passed){
+    console.log("he is passed ");
+}
+else{
+    console.log("he is failed ");
+}
