53c616e3fdb2efa318749faa666b0418767a97f9
Nom-an <nomanvi123@gmail.com>
2026-07-26 18:56:17 +0600

    comparison

---
+++ comparison.js
@@ -0,0 +1,14 @@
+var A = 10;
+var B = 20;
+
+console.log(A>B);
+
+A = 20;
+B = 20;
+
+console.log(A==B);
+
+A = 30;
+B = 40;
+
+console.log(B>=B);
---
+++ shortHand.js
@@ -0,0 +1,9 @@
+// addition
+var tv = 2000;
+ tv += 1000;
+
+ console.log(tv);
+//   multiply
+ tv *= 2;
+ console.log(tv);
+
