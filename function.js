5092ab7d18a8de95bfa9efa3698607f5764102b1
Nom-an <nomanvi123@gmail.com>
2026-07-28 20:43:05 +0600

    function

---
+++ function.js
@@ -0,0 +1,15 @@
+function hello (){
+    console.log("I'm a function");
+
+}
+
+hello();
+
+
+function additionMachine (input1, input2){
+    let sum = input1 + input2;
+    console.log("The sum is : " + sum);
+
+}
+
+additionMachine(10,23);
