c0a68fdfa0d4fbd5d7cce97ae77c1a08e6e3c979
Nom-an <nomanvi123@gmail.com>
2026-07-28 06:02:27 +0600

    practice

---
+++ loop.js
@@ -0,0 +1,12 @@
+let fruits = ['apple', 'banana', 'guava', 'mango']
+
+
+for( const fruit of fruits){
+        console.log(fruit);
+    if(fruit == 'banana'){
+        console.log("banana paisi to ");
+    }
+
+
+}
+
---
+++ whileDowhile.js
@@ -0,0 +1,18 @@
+ let n= 0;
+while ( n < 5){
+    console.log("print");
+    n++;
+}
+
+console.log("ki khobor");
+
+
+let m=0;
+do{
+    console.log("print");
+        m++;
+
+}
+while(m<5){
+    return 0;
+}
