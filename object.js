2256d1437be09907e2044e743e587310375832b9
Nom-an <nomanvi123@gmail.com>
2026-07-28 06:57:58 +0600

    creating object

---
+++ object.js
@@ -0,0 +1,16 @@
+let bottle = {
+    brand:'jibon',
+    price: 20,
+    color: 'white',
+    isBattle : true 
+}
+
+let subject = {
+    name: 'bangla',
+    teacher : 'ramisha mam',
+    chapter:'[first, second , third',
+    exams: {
+        name : 'final exam',
+        marks:100,
+    }
+}
