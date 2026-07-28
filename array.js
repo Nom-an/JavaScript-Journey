ca06ed7a8d15e09d7bf9641e8eb3944a98cb1127
Nom-an <nomanvi123@gmail.com>
2026-07-28 06:11:56 +0600

    playing with array

---
+++ array.js
@@ -0,0 +1,15 @@
+let fruits = [10, 30, 39, 49, 45];
+
+for ( let i = 0 ; i<fruits[i] ; i++){
+    console.log(fruits[i]);
+}
+
+let number = [ 10,20,33,23,2,42,2];
+number.push(32);
+console.log(number);
+number.pop();
+console.log(number)
+// remove from first using shitf 
+
+number.shift();
+console.log(number);
