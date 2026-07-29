2b31cdc19bacf2b32d11d4a2cf34f14f3b028ac6
Nom-an <nomanvi123@gmail.com>
2026-07-29 23:23:29 +0600

    use function inside object

---
+++ objects.js
@@ -0,0 +1,21 @@
+const student = {
+    name: 'Noman',
+    age: 24,
+    university: 'UIU',
+
+    car:{
+        owner: 'Elon Mask',
+        brand: 'Tesla',
+        model:'2026'
+    }
+    ,
+
+    act :function(){
+        console.log("Elon Mask is the ownner");
+
+    }
+}
+
+console.log(student);
+
+console.log(student.act());
