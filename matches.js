8ff90d1c371dc6e9bba9aeaf4dceaa2a12876137
Nom-an <nomanvi123@gmail.com>
2026-07-29 23:39:46 +0600

    search

---
+++ matches.js
@@ -0,0 +1,23 @@
+const numbers = [12,13,76,35,34,22];
+
+for ( number of numbers){
+    console.log(number);
+}
+
+
+const products=[
+    {id:1, name: 'walton', price: 20000},
+    {id:2, name: 'iphone', price: 23300},
+    {id:3, name: 'samphony', price: 25400},
+    {id:4, name: 'vivo', price: 204430},
+    {id:5, name: 'oppo', price: 20032},
+    {id:6, name: 'itel', price: 43000},
+    {id:7, name: 'google', price: 64000},
+    {id:8, name: 'samsung', price: 533000},
+    {id:9, name: 'lenevo', price: 33000},
+    {id:10, name: 'lava', price: 585000},
+]
+
+function matches(name, search){
+    
+}
